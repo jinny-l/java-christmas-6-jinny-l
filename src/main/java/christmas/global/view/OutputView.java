9b6e18535776494c3686a61fe15d2fe78bdaf962
jinny-l <jinny.l.dev@gmail.com>
2023-11-12 01:06:41 +0900

    ✨feat: 이벤트 플래너 시작 메시지 출력 기능 구현

---
+++ src/main/java/christmas/global/view/OutputView.java
@@ -0,0 +1,8 @@
+package christmas.global.view;
+
+public class OutputView {
+
+    public static void printGreetingMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+}
