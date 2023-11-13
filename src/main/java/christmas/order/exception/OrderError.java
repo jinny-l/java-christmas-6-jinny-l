932aa087f8e30d6352780da723b3e7a0e38ce733
jinny-l <jinny.l.dev@gmail.com>
2023-11-13 21:49:24 +0900

    🐛fix: 요구사항에 맞게 에러 메시지 수정

--- src/main/java/christmas/order/exception/OrderError.java
+++ src/main/java/christmas/order/exception/OrderError.java
@@ -7,7 +7,7 @@ public enum OrderError implements Error {
     INVALID_FORMAT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     INVALID_AMOUNT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     DUPLICATE_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    INVALID_MENU("메뉴에 없는 주문입니다."),
+    INVALID_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ALL_DRINK_ORDER("음료만 주문 시, 주문할 수 없습니다."),
     EXCEEDED_MAX_ORDER_AMOUNT("메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
 
