ff15469721982226c90723017a76bb792c42503e
jinny-l <jinny.l.dev@gmail.com>
2023-11-15 22:04:46 +0900

    ♻️refactor: 필요없는 코드 삭제

--- src/main/java/christmas/date/dto/VisitDayRequest.java
+++ src/main/java/christmas/date/dto/VisitDayRequest.java
@@ -5,13 +5,6 @@ import christmas.global.exception.ChristmasPromotionException;
 
 public record VisitDayRequest(int dayOfMonth) {
 
-    private static final int EVENT_START_DAY = 1;
-    private static final int EVENT_END_DAY = 31;
-
-    public VisitDayRequest {
-        validateDayOfMonth(dayOfMonth);
-    }
-
     public static VisitDayRequest from(String input) {
         int dayOfMonth = validateAndParseInt(input);
         return new VisitDayRequest(dayOfMonth);
@@ -21,12 +14,6 @@ public record VisitDayRequest(int dayOfMonth) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new ChristmasPromotionException(EventDateError.INVALID_DAY);
-        }
-    }
-
-    private void validateDayOfMonth(int dayOfMonth) {
-        if (dayOfMonth < EVENT_START_DAY || dayOfMonth > EVENT_END_DAY) {
             throw new ChristmasPromotionException(EventDateError.INVALID_DAY);
         }
     }
